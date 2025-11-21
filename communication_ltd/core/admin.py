275c1bd636044223924afcf2ec44875472679170
Yuval <yuvaloren25@hotmail.com>
2025-11-21 12:13:06 +0200

    Added tables and website

--- communication_ltd/core/admin.py
+++ communication_ltd/core/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import Sector, Package, Customer
 
-# Register your models here.
+admin.site.register(Sector)
+admin.site.register(Package)
+admin.site.register(Customer)
