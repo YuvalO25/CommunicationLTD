489aea45d912aa0a2c1d6f994b86285ceb0eb6a5
dvir berdugo <dvirbar272@gmail.com>
2025-12-08 22:07:49 +0200

    8.12 22:07

--- backend/users/views.py
+++ backend/users/views.py
@@ -1,3 +1,22 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.shortcuts import render, redirect
+from django.contrib import messages
+from django.contrib.auth import authenticate, login
+
+def login_view(request):
+    if request.method == "POST":
+        username = request.POST.get("username")
+        password = request.POST.get("password")
+
+        user = authenticate(request, username=username, password=password)
+
+        if user is not None:
+            login(request, user)
+            return redirect('/admin/')  # זמני — נחליף ל-dashboard בהמשך
+        else:
+            messages.error(request, "Incorrect username or password")
+            return render(request, 'users/login.html')
+
+    return render(request, 'users/login.html')
